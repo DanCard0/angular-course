ed1af7566581d12a2e69409d5ca27a7d58159e33
Daniel Cardozo <daniel.cardozo0621@gmail.com>
2017-03-28 07:43:10 -0500

    factories tutorial partially added

---
+++ factories/controller.js
@@ -0,0 +1,53 @@
+angular.module("ToDoList",["LocalStorageModule"])
+
+.factory("ToDoService", function(localStorageService){
+	var toDoService = {};
+
+	toDoService.key = "angular-todolist";
+	if(localStorageService.get(toDoService.key)){
+		toDoService.activities = localStorageService.get(toDoService.key);
+	}
+	else {
+		toDoService.activities = [];
+	}
+
+	toDoService.add = function(newActv){
+		toDoService.activities.push(newActv);
+		toDoService.updaLocalStorage();
+	};
+
+	toDoService.updaLocalStorage = function(){
+		localStorageService.set(toDoService.key, toDoService.activities);
+	};
+
+	toDoService.clean = function(){
+		toDoService.activities = []:
+		toDoService.updaLocalStorage();
+	};
+
+	toDoService.getAll = function(){
+		return toDoService.activities;
+	};
+
+	toDoService.removeItem = function(item){
+		toDoService.activities = toDoService.activities.filter(function(activity){
+			return activity !== item; // Itera, eliminando al activity que sea igual al ítem pasado por parámetro
+		});
+		toDoService.updaLocalStorage();
+		return toDoService.getAll();
+	}
+
+	return toDoService();
+})
+
+.controller("ToDoController", function($scope, ToDoService){
+
+	$scope.todo = ToDoService.getAll();
+	$scope.addActv = function(){
+		ToDoService.add(newActv);
+		$scope.newActv = {};
+	}
+	$scope.removeActv = function(item){
+		ToDoService.removeItem(item);
+	}
+});
